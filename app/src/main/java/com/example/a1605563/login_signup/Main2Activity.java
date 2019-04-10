da225ccf57a66a3d7e7f286791f67fa88807f21e
Surajnik1 <1605563@kiit.ac.in>
2019-04-10 23:01:04 +0530

    2nd branch

--- app/src/main/java/com/example/a1605563/login_signup/Main2Activity.java
+++ app/src/main/java/com/example/a1605563/login_signup/Main2Activity.java
@@ -1,6 +1,11 @@
 package com.example.a1605563.login_signup;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,25 +15,67 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Thread background = new Thread() {
-            public void run() {
-                try {
-                    // Thread will sleep for 5 seconds
-                    sleep(2*1000);
+        if(!isConnected(Main2Activity.this))
+            buildDialog(Main2Activity.this).show();
 
-                    // After 5 seconds redirect to another intent
-                    Intent i=new Intent(getBaseContext(),ImageSlider.class);
-                    startActivity(i);
+        else
+            {
+            Thread background = new Thread() {
+                public void run() {
+                    try {
+                        // Thread will sleep for 5 seconds
+                        sleep(2 * 1000);
 
-                    //Remove activity
-                    finish();
-                } catch (Exception e) {
+                        // After 5 seconds redirect to another intent
+                        Intent i = new Intent(getBaseContext(), ImageSlider.class);
+                        startActivity(i);
+
+                        //Remove activity
+                        finish();
+                    } catch (Exception e) {
+                    }
                 }
+
+            };
+            // start thread
+            background.start();
+        }
+
+    }
+    // if the mobile is connected or not
+    public boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting()))
+                return true;
+            else
+                return false;
+        } else
+        return false;
+    }
+
+    // creating a alert dialog box if internet is off
+    public AlertDialog.Builder buildDialog(Context c) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("You need to have Mobile Data or wifi to access this. Press ok to Exit");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                finish();
             }
-        };
-        // start thread
-        background.start();
+        });
 
-
+        return builder;
     }
 }
